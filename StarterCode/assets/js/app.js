2a905a35ba1ae8da90585e34f3444ac3b4fbcf44
njameel632 <57690530+njameel632@users.noreply.github.com>
2020-04-17 14:38:37 -0400

    Updating Files

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,8 +1,20 @@
 // @TODO: YOUR CODE HERE!
 //Chart Parameters
 
-var svgWidth = 960;
+var svgWidth = 690;
 var svgHeight = 500;
+
+var margin = {
+  top: 60,
+  right: 60,
+  bottom: 60,
+  left: 60,
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
+var padding = 50;
 
 // Creating SVG element
 
@@ -12,6 +24,113 @@ var svg = d3
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-d3.csv("/data/data.csv").then(function (data) {
-  console.log(data);
-});
+var chartGroup = svg
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+// Importing Data
+d3.csv("assets/data/data.csv").then(function (censusdata) {
+  console.log(censusdata);
+
+  // Parsing Data
+
+  censusdata.forEach(function(data){
+    data.poverty = +data.poverty;
+    data.healthcare = +data.healthcare;
+    data.income = +data.income;
+    data.obesity = + data.obesity;
+    data.age = +data.age;
+    data.smokes = +data.smokes;
+    
+
+  })
+
+    //Creating Scales
+
+    var x_scale = d3.scaleLinear()
+      .domain([8, d3.max(censusdata, d => d.poverty) + 2])
+      .range([0, chartWidth])
+
+    var y_scale = d3.scaleLinear()
+      .domain([4, d3.max(censusdata, d => d.healthcare) + 2])
+      .range([chartHeight, 0]);
+
+    // 
+
+  
+    // Creating Axis
+
+    var bottomaxis = d3.axisBottom(x_scale)
+    .tickFormat(function(d){
+      return d + '%'
+    })
+    
+    var leftaxis = d3.axisLeft(y_scale)
+    .tickFormat(function(d){
+      return d + '%'
+    })
+
+    // Appending The axes to The Chart Group
+
+    chartGroup.append('g')
+    .attr('transform', `translate(0, ${chartHeight})`)
+    .call(bottomaxis);
+
+    chartGroup.append('g')
+    .call(leftaxis);
+
+    // Creating Cirlces for Scatter Plot
+
+     var circlesGroup = chartGroup.selectAll('circle')
+      .data(censusdata)
+      .enter()
+      .append('circle')
+      .attr('cx', d => x_scale(d.poverty))
+      .attr('cy', d => y_scale(d.healthcare))
+      .attr('r', '10')
+      .attr('fill', 'green')
+      
+      
+    var textGroup = chartGroup.selectAll('text')
+      .exit()
+      .data(censusdata)
+      .enter()
+      .append('text')
+      .text(function(d){
+        return `${d.abbr}`
+      })
+      .attr('x', d => x_scale(d.poverty))
+      .attr('y', d => y_scale(d.healthcare))
+      .attr('text-anchor', 'middle')
+      .attr('font-family', 'sans-serif')
+      .attr('font-size', '10px')
+      .attr('fill', 'white');
+    
+    
+// Appending ToolTip to Div
+    
+var toolTip = d3.tip()
+    .attr('class', 'tooltip')
+    .offset([80, -60])
+    .html(function(d){
+      return (`State: ${d.state}<br>Poverty: ${d.poverty}<br>Lacks Healthcare: ${d.healthcare}`)
+    })
+
+// Creating A tooltip in chartGroup
+
+chartGroup.call(toolTip);
+
+// Create Event Listeners
+circlesGroup.on('click', function(data, index){
+  toolTip.show(data, this);
+})
+
+// On Mouseout
+
+.on('mouseout', function(data, index){
+  toolTip.hide(data);
+})
+
+
+
+
+})
